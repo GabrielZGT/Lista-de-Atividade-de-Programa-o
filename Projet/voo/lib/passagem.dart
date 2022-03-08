d6929fd9412a4132f36734f55a9780a640c899bc
Gabriel Filipe <99221442+GabrielZGT@users.noreply.github.com>
2022-03-08 19:28:55 -0300

    Add files via upload

---
+++ Projet/voo/lib/passagem.dart
@@ -0,0 +1,9 @@
+import 'package:voo/passageiro.dart';
+
+class Passagem{
+  int numero =0;
+  int poltrona = 0;
+  String voo = '';
+  DateTime? hora;
+  Passageiro? passageiro;
+}
---
+++ Projet/voo/lib/voo.dart
@@ -0,0 +1,7 @@
+import 'package:voo/passagem.dart';
+
+class Aviao {
+  String aviao = '';
+  String destino = '';
+  List <Passagem>?passageiros; 
+}
